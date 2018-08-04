883cda0325345bdf6fc349a8bd170684e379fc95
Anne <Anne@jelly.local>
2018-08-03 23:14:45 -0700

    file-save format updatee

--- Scripts/log_chi_spec.sh
+++ Scripts/log_chi_spec.sh
@@ -14,8 +14,8 @@
  	radii=("30" "40" "50" "60")
  	energies=("5")
  	widths=("0.1" "0.3")
- 	patterns={"0" "0-4"}
- 	rates=("0.4" "0.7" "3" "10" )
+ 	patterns=("0" "0-4")
+ 	rates=("0.4" "0.7" )
 
 	#Title
 	echo "${obs_id}" >> delc_document
@@ -40,101 +40,101 @@
 						fi
 
 						#Clean documentation files.
-						rm ${radius}_${energy}-${filewidth}_${pattern}.xco
-						rm ${radius}_${energy}-${filewidth}_${pattern}.log
+						rm ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						rm ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.log
 						rm plot_${radius}_${energy}_${filewidth}_${pattern}_delc
-						rm zgauss_${radius}_${energy}-${filewidth}_${pattern}.log
-						rm log tbabs_${radius}_${energy}-${filewidth}_${pattern}.log
+						rm zgauss_${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.log
+						rm log tbabs_${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.log
 
 						# Look for files that correspond to the given parameters ands assign them to a variable.
-						datafile="$(ls | grep "${pattern}" | grep "$radius" | grep "20.fits")"
-						respdata="$(ls | grep "${pattern}"| grep "$radius" | grep ".rmf")"
-						arf="$(ls | grep "${pattern}" | grep "$radius" | grep ".arf")"
+						datafile="$(ls | grep "${pattern}" | grep "$radius" | grep "${rate//.}"| grep "20.fits")"
+						respdata="$(ls | grep "${pattern}"| grep "$radius" | grep "${rate//.}" | grep ".rmf")"
+						arf="$(ls | grep "${pattern}" | grep "$radius" | grep "${rate//.}" | grep ".arf")"
 						bgfile="pn_${pattern}_bgd2_pi.fits"
 
 
-						echo "query yes" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "query yes" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 
 						#Put in the corredct files and output them to a parameter specific XSPEC file.
-						echo "data $datafile" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "back $bgfile" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "resp $respdata" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "arf $arf" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "ignore **-2.0 10.0-**" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "ignore bad" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "data $datafile" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "back $bgfile" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "resp $respdata" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "arf $arf" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "ignore **-2.0 10.0-**" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "ignore bad" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 
 
 
 						# Define the model
-						echo "model tbabs*cutoffpl" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "model tbabs*cutoffpl" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 						#Absoprtion
-						echo "1.0 ,0.1" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "1.0 ,0.1" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 						#powerlaw index/gamma
-						echo "2, 0.01" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "2, 0.01" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 						#Cuttoff energy
-						echo "15, 0.01" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "15, 0.01" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 						#Normalization
-						echo "1.E-4 1E-5" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "fit" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "1.E-4 1E-5" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "fit" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 
 						#Create a log file, and list spectrum characteristics
-						echo "log tbabs_${radius}_${energy}-${filewidth}_${pattern}.log" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "show all" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "log none" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						filename="/Users/Anne/Documents/Caltech/${obs_id}/ULX/tbabs_${radius}_${energy}-${filewidth}_${pattern}.log"
-						tbabs_chi=$(awk  '/Fit statistic : Chi-Squared =/ {print $6}' $filename)
+						echo "log tbabs_${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.log" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "show all" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "log none" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 
 						# Save the best-fit model, what is rebin 3 10?
-						echo "setplot energy" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "setplot rebin 3 10" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "plot ldata" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "setplot energy" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "setplot rebin 3 10" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "plot ldata" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 
-						echo "hardcopy plot_${radius}_${pattern} color" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "addc 3 zgauss" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "hardcopy plot_${radius}_${pattern} color" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "addc 3 zgauss" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 						#energy
-						echo "$energy 0.01 0.5 0.5 8.0 8.0" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "$energy 0.01 0.5 0.5 8.0 8.0" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 						#width
-						echo "$width, -1" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "$width, -1" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 						#normalization
-						echo "0.002,-1" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "0.002,-1" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 
-						echo "-1e-6 1e-7 -1e10 -1e10 -1e-10 -1e-10" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "fit" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "-1e-6 1e-7 -1e10 -1e10 -1e-10 -1e-10" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "fit" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 						#fix the plot.
-						echo "setplot energy" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "setplot rebin 3 10" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "plot ld delc" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						#echo "save all ${radius}_plot.xcm" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "setplot energy" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "setplot rebin 3 10" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "plot ld delc" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						#echo "save all ${radius}_plot.xcm" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 						#Create a log file, and list spectrum characteristics
-						echo "log zgauss_${radius}_${energy}-${filewidth}_${pattern}.log" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "show all" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "log none" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "log zgauss_${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.log" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "show all" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "log none" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 
 						#save the plot
-						echo "hardcopy plot_${radius}_${energy}_${filewidth}_${pattern}_delc color" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
-						echo "exit" >> ${radius}_${energy}-${filewidth}_${pattern}.xco
+						echo "hardcopy plot_${radius}_${energy}_${filewidth}_${pattern}_delc color" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
+						echo "exit" >> ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 
 						#launch XSPEC
-						xspec - ${radius}_${energy}-${filewidth}_${pattern}.xco
+						xspec - ${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.xco
 
 						#strip the chi squared value and perform arithmetic to get delc.
-						tbabs_file="/Users/Anne/Documents/Caltech/${obs_id}/ULX/tbabs_${radius}_${energy}-${filewidth}_${pattern}.log"
+						tbabs_file="/Users/Anne/Documents/Caltech/${obs_id}/ULX/tbabs_${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.log"
 						tbabs_chi=$(awk  '/Fit statistic : Chi-Squared =/ {print $6}' $tbabs_file)
 
-						zgauss_file="/Users/Anne/Documents/Caltech/${obs_id}/ULX/zgauss_${radius}_${energy}-${filewidth}_${pattern}.log"
+						zgauss_file="/Users/Anne/Documents/Caltech/${obs_id}/ULX/zgauss_${radius}_${energy}-${filewidth}_${pattern}_${rate//.}.log"
 						zgauss_chi=$(awk  '/Fit statistic : Chi-Squared =/ {print $6}' $zgauss_file)
 
 
 						delc=$(bc <<< "${tbabs_chi}-${zgauss_chi}")
-						echo "$delc ${radius} ${energy} ${width} ${pattern}	" >> delc_document.txt
+						echo "$delc ${radius} ${energy} ${width} ${pattern} ${rate//.}	" >> delc_document.txt
 
 
 
 
+					done
 				done
 			done
 		done
 	done
+
 	#Easu indicators to format and observation.
 	echo "Observation ID operated on: $obs_id"
 	echo "Files ordered such that [radius]_[energy]-[width]_[pattern]"
--- Scripts/rate_reduction.sh
+++ Scripts/rate_reduction.sh
@@ -37,9 +37,9 @@ read -p "WARNING! Correct Observation and Detector coordinates (via DS9) must be
 
 
 #List of parameters that we want to vary.
-radii=("30" "40" "50" "60")
+radii=("30" "40" "50")
 patterns=("0-4" "0")
-rates=("0.4" "0.7" "3" "10" )
+rates=("0.4" "0.7")
 #For loop for combinations of varied parameters.
 for pattern in ${patterns[@]}; do
 	if [ $pattern="0" ]
@@ -60,35 +60,35 @@ for pattern in ${patterns[@]}; do
 			detector_radius=$(expr $radius \* 20)
 
 			#I usually don't automate the line below, but we don't necessarily care about the light curve in this script.
-			evselect table=pn.fits withfilteredset=yes expression='(PATTERN <= '"${patnumber}"')&&(PI in [10000:12000])&&#XMMEA_EP' filteredset=pn_pat${pattern}_en10-12.fits filtertype=expression keepfilteroutput=yes updateexposure=yes filterexposure=yes withrateset=yes rateset=pn_pat${pattern}_en10-12_ltcrv.fits maketimecolumn=yes timecolumn=TIME timebinsize=100 makeratecolumn=yes
+			evselect table=pn.fits withfilteredset=yes expression='(PATTERN <= '"${patnumber}"')&&(PI in [10000:12000])&&#XMMEA_EP' filteredset=pn_en10-12_pat${pattern}_${radius}_${rate//.}.fits filtertype=expression keepfilteroutput=yes updateexposure=yes filterexposure=yes withrateset=yes rateset=pn_ltcrv_en10-12_pat${pattern}_${radius}_${rate//.}.fits maketimecolumn=yes timecolumn=TIME timebinsize=100 makeratecolumn=yes
 
 
 			# For further detail, the syntax of the below $rate variable is ${string//substring/replacement},
 			# where apparently the lack of the final forward slash and replacement string are interpreted as delete.
-			tabgtigen table=pn_pat${pattern}_en10-12_ltcrv.fits expression='RATE<='"${rate//.}"'' gtiset=lowbg_gti.fits
+			tabgtigen table=pn_src_ltcrv_en10-12_pat${pattern}_${radius}_${rate//.}.fits expression='RATE<='"${rate//.}"'' gtiset=lowbg_gti.fits
 
 			
 
 			#spectrum
-			evselect table='pn.fits' withfilteredset=yes expression='(PATTERN<='"${patnumber}"')&&(PI in [200:10000])&&#XMMEA_EP&&gti(lowbg_gti.fits,TIME)&&((X,Y) in CIRCLE(24125,24618,'"${detector_radius}"'))' filteredset=pn_pat${pattern}_en0.2-10_src_${radius}_${rate//.}.fits filtertype=expression keepfilteroutput=yes updateexposure=yes filterexposure=yes withrateset=yes rateset=pn_pat${pattern}_en0.2-10_src_${radius}_${rate//.}_ltcrv.fits maketimecolumn=yes timecolumn=TIME timebinsize=0.074 makeratecolumn=yes energycolumn='PI' withspectrumset=yes spectrumset='pn_pat'"${pattern}"'_src_'"${radius}"'_'"${rate//.}"'_pi.fits' spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=20479
+			evselect table='pn.fits' withfilteredset=yes expression='(PATTERN<='"${patnumber}"')&&(PI in [200:10000])&&#XMMEA_EP&&gti(lowbg_gti.fits,TIME)&&((X,Y) in CIRCLE(24125,24618,'"${detector_radius}"'))' filteredset=pn_src_en0.2-10_pat${pattern}_${radius}_${rate//.}.fits filtertype=expression keepfilteroutput=yes updateexposure=yes filterexposure=yes withrateset=yes rateset=pn_src_ltcrv_en0.2-10_${pattern}_${radius}_${rate//.}.fits maketimecolumn=yes timecolumn=TIME timebinsize=0.074 makeratecolumn=yes energycolumn='PI' withspectrumset=yes spectrumset='pn_src_pat'"${pattern}"'_'"${radius}"'_'"${rate//.}"'_pi.fits' spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=20479
 
 			#background
-			evselect table='pn.fits' withfilteredset=yes expression='(PATTERN<='"${patnumber}"')&&(PI in [200:10000])&&#XMMEA_EP&&gti(lowbg_gti.fits,TIME)&&((X,Y) in CIRCLE(23848,29389,1000))' filteredset=pn_pat${pattern}_en0.2-10_bgd2.fits filtertype=expression keepfilteroutput=yes updateexposure=yes filterexposure=yes withrateset=yes rateset=pn_pat${pattern}_en0.2-10_bgd2_ltcrv.fits maketimecolumn=yes timecolumn=TIME timebinsize=0.074 makerate//.column=yes energycolumn='PI' withspectrumset=yes spectrumset='pn_pat'"${pattern}"'_bgd2_pi.fits' spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=20479
+			evselect table='pn.fits' withfilteredset=yes expression='(PATTERN<='"${patnumber}"')&&(PI in [200:10000])&&#XMMEA_EP&&gti(lowbg_gti.fits,TIME)&&((X,Y) in CIRCLE(23848,29389,1000))' filteredset=pn_bgd2_en0.2-10_pat${pattern}_${radius}_${rate//.}.fits filtertype=expression keepfilteroutput=yes updateexposure=yes filterexposure=yes withrateset=yes rateset=pn_src_bgd2_ltcrv_en0.2-10_pat${pattern}_${radius}_${rate//.}.fits maketimecolumn=yes timecolumn=TIME timebinsize=0.074 makerate//.column=yes energycolumn='PI' withspectrumset=yes spectrumset='pn_bgd2_pat'"${pattern}"'_'"${radius}"'_'"${rate//.}"'_pi.fits' spectralbinsize=5 withspecranges=yes specchannelmin=0 specchannelmax=20479
 
 			#backscale
-			backscale spectrumset='pn_pat'"${pattern}"'_src_'"${radius}"'_'"${rate//.}"'_pi.fits'
-			backscale spectrumset='pn_pat'"${pattern}"'_bgd2_pi.fits'
+			backscale spectrumset='pn_src_pat'"${pattern}"'_'"${radius}"'_'"${rate//.}"'_pi.fits'
+			backscale spectrumset='pn_bgd2_pat'"${pattern}"'_'"${radius}"'_'"${rate//.}"'_pi.fits'
 
 			#rmf
-			rmfgen rmfset=pn_pat${pattern}_src_${radius}_${rate//.}.rmf spectrumset='pn_pat'"${pattern}"'_src_'"${radius}"'_'"${rate//.}"'_pi.fits'
+			rmfgen rmfset=pn_src_pat${pattern}_${radius}_${rate//.}.rmf spectrumset='pn_src_pat'"${pattern}"''"${radius}"'_'"${rate//.}"'_pi.fits'
 
 			#arf
-			arfgen arfset=pn_pat${pattern}_src_${radius}_${rate//.}.arf spectrumset='pn_pat'"${pattern}"'_src_'"${radius}"'_'"${rate//.}"'_pi.fits' withrmfset=yes rmfset=pn_pat${pattern}_src_${radius}_${rate//.}.rmf
+			arfgen arfset=pn_src_pat${pattern}_${radius}_${rate//.}.arf spectrumset='pn_src_pat'"${pattern}"'_'"${radius}"'_'"${rate//.}"'_pi.fits' withrmfset=yes rmfset=pn_src_pat${pattern}_${radius}_${rate//.}.rmf
 
 			#check if grouped spectrum exists. If not, group the spectrum!
-			if [ ! -f pn_pat${pattern}_src_${radius}_${rate//.}_pi_20.fits ] || [! -f pn_pat${pattern}_src_${radius}_${rate//.}_pi_1.fits] ; then
-				grppha pn_pat${pattern}_src_${radius}_${rate//.}_pi.fits pn_pat${pattern}_src_${radius}_${rate//.}_pi_20.fits "group min 20" exit
-				grppha pn_pat${pattern}_src_${radius}_${rate//.}_pi.fits pn_pat${pattern}_src_${radius}_${rate//.}_pi_1.fits "group min 1" exit
+			if [ ! -f pn_src_pat${pattern}_${radius}_${rate//.}_pi_20.fits ] || [! -f pn_src_pat${pattern}_${radius}_${rate//.}_pi_1.fits] ; then
+				grppha pn_src_pat${pattern}_${radius}_${rate//.}_pi.fits pn_src_pat${pattern}_${radius}_${rate//.}_pi_20.fits "group min 20" exit
+				grppha pn_src_pat${pattern}_${radius}_${rate//.}_pi.fits pn_src_pat${pattern}_${radius}_${rate//.}_pi_1.fits "group min 1" exit
     			
 			fi
 
